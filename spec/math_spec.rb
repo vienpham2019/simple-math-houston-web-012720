9789d4386e6309e9801c9b5458984c7ee8516f99
vien pham <vienpham2019@gmail.com>
2020-01-28 19:27:24 +0000

    Automatically backed up by Learn

--- spec/math_spec.rb
+++ spec/math_spec.rb
@@ -38,7 +38,7 @@ end
 
 describe "order_of_operations" do
   it "uses parenthesis to set the order of operations properly" do
-    expect(order_of_operation(7, 43, 23, 83)).to eq(18)
+    expect(order_of_operation(7,28,48,)).to eq(18)
   end
 end
 
